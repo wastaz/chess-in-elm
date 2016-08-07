4fc6b0e8058420b292bdde6b1267935e439abbbb
Fredrik Forssen <fredde@grodslok.net>
2016-08-07 10:56:22 +0200

    Draw all pieces

--- main.elm
+++ main.elm
@@ -2,7 +2,7 @@ import Html exposing (text)
 import Html.App as App
 import Collage as GC
 import Collage exposing (defaultLine)
-import Element exposing (toHtml)
+import Element exposing (toHtml, image)
 import Color as Color
 
 type ChessPiece
@@ -22,9 +22,17 @@ type alias Position = (Int, Int)
 type alias Piece = (Player, ChessPiece, Position)
 
 initialBoard =
-    [ (Black, Pawn, (1, 2)),
-      (Black, Pawn, (2, 2))
-    ]
+    List.concat
+        [
+            List.map (\i -> (White, Pawn, (i, 1))) [0 .. 7],
+            List.map (\i -> (Black, Pawn, (i, 6))) [0 .. 7],
+            [ (White, 0), (Black, 7)]
+            |> List.map (\(clr, y) ->
+                    [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+                    |> List.indexedMap (\x pc -> (clr, pc, (x, y)))
+                )
+            |> List.concat
+        ]
 
 main =
     App.beginnerProgram { model = initialBoard, view = view, update = update }
@@ -42,7 +50,7 @@ other player =
 toColor player =
     case player of
         White -> Color.white
-        Black -> Color.black
+        Black -> Color.charcoal
 
 makeRow clr =
     [0..7]
@@ -57,7 +65,7 @@ makeRow clr =
         )
     |> GC.collage 800 100
 
-makeBoard clr =
+drawBoard clr =
     [0..7]
     |> List.map (\i ->
             let startColor = if i % 2 == 0 then clr else other clr
@@ -68,14 +76,48 @@ makeBoard clr =
         )
     |> GC.collage 800 800
 
-drawBoard =
-    [
-        GC.rect 805 805 |> GC.outlined { defaultLine | width = 5 },
-        makeBoard White |> GC.toForm
-    ]
-    |> GC.collage 810 810
+positionToCoords (x, y) =
+    let
+        cx = (toFloat x) * 100.0 - 350.0
+        cy = (toFloat y) * 100.0 - 350.0
+    in
+        (cx, cy)
 
+imageFor player piece =
+    let
+        playerstr =
+            case player of
+                Black -> "black"
+                White -> "white"
+        piecestr =
+            case piece of
+                Pawn -> "pawn"
+                Rook -> "rook"
+                Knight -> "knight"
+                Bishop -> "bishop"
+                Queen -> "queen"
+                King -> "king"
+    in
+        "assets/" ++ piecestr ++ "_" ++ playerstr ++ ".png"
+
+drawPieces pcs =
+    pcs
+    |> List.map (\(player, piece, position) ->
+            imageFor player piece
+            |> image 50 50
+            |> GC.toForm
+            |> GC.move (positionToCoords position)
+        )
 
 view model =
-    drawBoard
+    List.concat
+        [
+            [
+                GC.rect 805 805 |> GC.outlined { defaultLine | width = 5 },
+                drawBoard Black |> GC.toForm
+            ],
+            drawPieces model
+        ]
+
+    |> GC.collage 810 810
     |> toHtml
